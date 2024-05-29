c7dafe2d602db1957a586829d260a575b6117851
DavidViklund <david.viklund@chasacademy.se>
2024-05-29 11:17:05 +0200

    Adderat pagelayout

---
+++ webshop/src/components/PageLayout.jsx
@@ -0,0 +1,7 @@
+const PageLayout = () => {
+  return (
+    <div className=" flex max-w-[1440px] sm:px-4 md:px-8 lg:px-40 lg:py-[100px] sm:py-[100px] md:py-[100px]"></div>
+  );
+};
+
+export default PageLayout;
