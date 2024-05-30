85404a5ba85f2316b2890c76ebd1691f1598472c
joakimstrandlund <145377240+joakimstrandlund@users.noreply.github.com>
2024-05-30 10:55:12 +0200

    La till produktknappar till produktsidan/kategorier

--- webshop/src/api/dataFetching.js
+++ webshop/src/api/dataFetching.js
@@ -1,7 +1,15 @@
 export const getProducts = async () => {
   const response = await fetch('https://fakestoreapi.com/products');
   if (!response.ok) {
-    throw new Error('Error');
+    throw new Error('Error fetching products');
+  }
+  return response.json();
+};
+
+export const getCategories = async () => {
+  const response = await fetch('https://fakestoreapi.com/products/categories');
+  if (!response.ok) {
+    throw new Error('Error fetching categories');
   }
   return response.json();
 };
