d2223c59a665621fbb7f8ec5ba54d49490ab52a8
jonasbergmar <jonas.bergmar@hotmail.com>
2024-06-03 11:06:31 +0200

    Klara med navbar

--- webshop/src/components/NavBar.jsx
+++ webshop/src/components/NavBar.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import SecondaryButton from "./SecondaryButton";
 
 const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -18,19 +19,27 @@ const NavBar = () => {
         <Link className="text-2xl" to="/">
           <h4>HaulMart</h4>
         </Link>
-        <div className="iconBox lg:hidden" onClick={toggleMenu} style={{ cursor: 'pointer' }}>
-          <span className="material-symbols-outlined flex items-center justify-center w-10 h-10">{isOpen ? 'menu_open' : 'menu'}</span>
+        <div
+          className="iconBox lg:hidden"
+          onClick={toggleMenu}
+          style={{ cursor: "pointer" }}
+        >
+          <span className="material-symbols-outlined flex items-center justify-center w-10 h-10">
+            {isOpen ? "menu_open" : "menu"}
+          </span>
         </div>
-        <div className="gap-2 hidden lg:flex">
+        <div className="  gap-2 hidden lg:flex lg:items-center">
           <Link className="navLink" to="/ProductPage" onClick={closeMenu}>
             ProductPage
           </Link>
           <Link className="navLink" to="/AboutUsPage" onClick={closeMenu}>
             AboutUsPage
           </Link>
-          <Link className="navLink" to="/CartPage" onClick={closeMenu}>
-            CartPage
-          </Link>
+          <SecondaryButton
+            to={"/CartPage"}
+            text={"Varukorg"}
+            icon={<span class="material-symbols-outlined">shopping_cart</span>}
+          ></SecondaryButton>
         </div>
       </div>
       {isOpen && (
@@ -44,6 +53,11 @@ const NavBar = () => {
           <Link className="navLink" to="/CartPage" onClick={closeMenu}>
             CartPage
           </Link>
+          <SecondaryButton
+            to={"/CartPage"}
+            text={"Varukorg"}
+            icon={<span class="material-symbols-outlined">shopping_cart</span>}
+          ></SecondaryButton>
         </div>
       )}
     </div>
