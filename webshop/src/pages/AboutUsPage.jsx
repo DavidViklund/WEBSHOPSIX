4749f43d6a8218cbf7964acf117135aa1a5ef60a
FridaLund <frida.lund@hasacademy.se>
2024-06-10 14:00:28 +0200

    Frida och David har fixat med About sidan. Dom har styrt upp presentations layout.

--- webshop/src/pages/AboutUsPage.jsx
+++ webshop/src/pages/AboutUsPage.jsx
@@ -9,20 +9,74 @@ const aboutUsPage = () => {
             <h4>Filippa</h4>
           </div>
         </div>
-        <div className=" flex flex-col gap-8">
-          <div>
-            <h3>Vår metod</h3>
-            <p>Text om teamet - rollfördelning, agila arbetsprocessen</p>
-          </div>
-          <div>
+        <div className=" flex flex-col gap-32">
+          <div className=" flex flex-col gap-4">
             <h3>Design</h3>
-            <p>Text om designbeslut</p>
+            <div className=" flex flex-col gap-2">
+              <div>
+                <img
+                  alt="chefpic"
+                  src="public/colours.jpeg"
+                  className=" h-auto w-[500px] rounded-xl"
+                />
+              </div>
+              <h5>Allmänt om designen - enkelt och UI-säkert formspråk.</h5>
+              <p>design 1</p>
+              <p>design 2</p>
+              <p>design 3</p>
+              <h5>Färger</h5>
+              <p>nånting om färg 1</p>
+              <h5>Font</h5>
+              <p>nånting om font 1</p>
+              <h5>Kod</h5>
+              <p>om kod 1</p>
+              <p>om kod 2</p>
+              <p>om kod 3</p>
+            </div>
           </div>
-          <div>
+          <div className=" flex flex-col gap-4">
+            <h3>Vår metod</h3>
+            <div>
+              <img
+                alt="militarypic"
+                src="public/military.jpg"
+                className=" h-auto w-[500px] rounded-xl"
+              />
+            </div>
+            <div className=" flex flex-col gap-2">
+              <h5>Rollfördelning</h5>
+              <p>rollfördelning 1</p>
+              <p>rollfördelning 2</p>
+              <p>rollfördelning 3</p>
+              <h5>Arbetsprocess och agil arbetsmetodik - utveckla mycket</h5>
+              <p>punkt 1</p>
+              <p>punkt 2</p>
+              <p>punkt 3</p>
+            </div>
+          </div>
+          <div className=" flex flex-col gap-4">
             <h3>Lärdomar</h3>
-            <p>
-              Text om lärdomar, vad som fungerade bra och vad som kan förbättras
-            </p>
+            <div>
+              <img
+                alt="chefpic"
+                src="public/chefs.jpg"
+                className=" h-auto w-[500px] rounded-xl"
+              />
+            </div>
+            <div className=" flex flex-col gap-2">
+              <h5>Vilka lärdomar tar vi med oss?</h5>
+              <p>lärdom 1</p>
+              <p>lärdom 2</p>
+              <p>lärdom 3</p>
+              <h5>Vad som fungerade bra?</h5>
+              <p>bra grej 1</p>
+              <p>bra grej 2</p>
+              <p>bra grej 3</p>
+              <h5>Vad hade kunnat förbättras?</h5>
+              <p>förbättring 1</p>
+              <p>förbättring 2</p>
+              <p>förbättring 3</p>
+            </div>
           </div>
         </div>
       </div>
