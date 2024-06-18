620b4c7d54dc4023e5bc15a9ed804f403f3ded73
FridaLund <frida.lund@hasacademy.se>
2024-06-18 23:44:19 +0200

    Synliggjort kommentarer som låg dolda när vi hade vår presentation. Det är dokumentation för arbetet.

--- webshop/src/pages/AboutUsPage.jsx
+++ webshop/src/pages/AboutUsPage.jsx
@@ -23,8 +23,11 @@ const aboutUsPage = () => {
               </div>
               <div className=" flex flex-col gap-2">
                 <h5>Allmänt om designen</h5>
-                {/*  <p>
-                  Vi har strävat efter en enkel och UI-säker design. Se <br />
+                <li className="font-bold">Enkelt och UI-säkert</li>
+                <p>
+                  Vi har strävat efter en enkel och UI-säker design. Detta för
+                  att inte lägga för mycket tid på designval men också göra det
+                  enklare att koda. Se
                   <a
                     href="https://www.figma.com/design/AjypPMUpBTH4BmJbJsJeW6/wireframe?node-id=0-1&t=zo5q35sq6M3VUGny-1"
                     target="_blank"
@@ -33,88 +36,89 @@ const aboutUsPage = () => {
                     Figma-filen
                   </a>{" "}
                   för utförlig motivering.
-                </p> */}
-                <li className="font-bold">Enkelt och UI-säkert</li>
+                </p>
                 <li className="font-bold">Använde färdigt designsystem</li>
-                {/* <p>Tack vare ett befintligt editerbart designsystem
-                kunde vi snabbt komma igång med designen och implementera
-                vårga färger, fonter etc.
-              </p> */}
+                <p>
+                  Tack vare att Jonas hade gjort ett befintligt editerbart
+                  designsystem kunde vi snabbt komma igång med designen och
+                  implementera vårga färger, fonter etc.
+                </p>
                 <li className="font-bold">Figma som ritning</li>
-                {/* <p>
+                <p>
                   Alla designskisser skapades i Figma och översattes därefter
                   till Tailwind. Det var smidigt att ha all info i Figmafilen
                   men något utmanande att översätta för en del. Jonas hade tex
                   preppat variabler för att underlätta letandet.
-                </p> */}
+                </p>
               </div>
               <div className=" flex flex-col gap-2">
                 <h5>Färger</h5>
                 <li className="font-bold">Mindre kognitiv belastning</li>
-                {/* <p>
-                Färgval gjordes för att minimera användarens kognitiva
-                belastning.
-              </p> */}
+                <p>
+                  Färgval gjordes för att minimera användarens kognitiva
+                  belastning.
+                </p>
                 <li className="font-bold">Tydligt CTA</li>
-                {/* <p>
-                Vi använde vår primära färg strategiskt för att framhäva viktiga
-                åtgärder (CTA) och undvika konkurrens med andra färger.
-              </p> */}
+                <p>
+                  Vi använde vår primära färg strategiskt för att framhäva
+                  viktiga åtgärder (CTA) och undvika konkurrens med andra
+                  färger.
+                </p>
                 <li className="font-bold">Varje färg har ett syfte</li>
-                {/* <p>
-                Färgerna valdes för att ha specifika funktioner som är tydliga
-                för användaren.
-              </p> */}
+                <p>
+                  Färgerna valdes för att ha specifika funktioner som är tydliga
+                  för användaren.
+                </p>
               </div>
               <div className=" flex flex-col gap-2">
                 <h5>Font</h5>
                 <li className="font-bold">Hög läsbarhet</li>
-                {/* <p>
-                Vi använde fonterna Montserrat och Roboto. Båda har raka linjer
-                och mjuka former, vilket gör dem lättlästa. 
-                Här la vi till en lineheight som främjar läsbarheten och matchar vårt
-                spacingsystem. 
-              </p> */}
+                <p>
+                  Vi använde fonterna Montserrat och Roboto. Båda har raka
+                  linjer och mjuka former, vilket gör dem lättlästa. Här la vi
+                  till en lineheight som främjar läsbarheten och matchar vårt
+                  spacingsystem.
+                </p>
                 <li className="font-bold">Ökad prestanda</li>
-                {/*  <p>
-                Vi valde en font som inte belastar prestandan, utan som laddas
-                direkt i moderna webbläsare.
-              </p> */}
+                <p>
+                  Vi valde en font som inte belastar prestandan, utan som laddas
+                  direkt i moderna webbläsare.
+                </p>
                 <li className="font-bold">Visuell hiearki</li>
-                {/* <p>Typsnitten stödjer en tydlig visuell hierarki.</p> */}
+                <p>Typsnitten stödjer en tydlig visuell hierarki.</p>
               </div>
               <div className=" flex flex-col gap-2">
                 <h5>Kod</h5>
                 <li className="font-bold">Styling med tailwind</li>
-                {/*  <p>
-                Vi använde Tailwind CSS för snabb och enkel styling baserat på
-                våra Figma-skisser.
-              </p> */}
+                <p>
+                  Vi använde Tailwind CSS för snabb och enkel styling baserat på
+                  våra Figma-skisser.
+                </p>
                 <li className="font-bold">Sorterat i mappar</li>
-                {/* <p>Koden är organiserad i strukturerade mappar.</p> */}
+                <p>Koden är organiserad i strukturerade mappar.</p>
                 <li className="font-bold">Följa Sagas praxis</li>
-                {/* <p>
-                Vi följde bästa praxis enligt hennes riktlinjer, inklusive
-                användning av arrow functions, korrekta importer/exporter,
-                återanvändbar och modulär kod, samt minimal kodmängd.
-              </p> */}
+                <p>
+                  Vi följde bästa praxis enligt hennes riktlinjer, inklusive
+                  användning av arrow functions, korrekta importer/exporter,
+                  återanvändbar och modulär kod, samt minimal kodmängd.
+                </p>
                 <li className="font-bold">Parkodning</li>
-                {/* <p>
-                Vi varierade mellan grupparbete, parkodning och enskilt arbete
-                beroende på behov.
-              </p> */}
+                <p>
+                  Vi varierade mellan grupparbete, parkodning och enskilt arbete
+                  beroende på behov.
+                </p>
               </div>
             </div>
           </div>
           <div className="flex flex-col gap-8 max-w-[500px]">
             <h3>Vår metod</h3>
-            {/* <p>
+            <p>
               Parkodning var särskilt bra när vi skulle pusha upp i början,
               kontrollera vilken gren vi var på och, mest nervöst av allt, pusha
               upp till main. Därför hade vi i början en person som ansvarade för
               alla pull requests. När viktiga ändringar skulle göras hjälpte vi
               varandra att dubbelkolla. Med tiden blev vi mer självsäkra!
-            </p> */}
+            </p>
             <div>
               <img
                 alt="militarypic"
@@ -132,25 +136,25 @@ const aboutUsPage = () => {
             <div className=" flex flex-col gap-2">
               <h5>Arbetsprocess och agil arbetsmetodik</h5>
               <li className="font-bold">GitHub - Kanban</li>
-              {/* <p>
+              <p>
                 Vi använde GitHub med Kanban för att organisera vårt arbete.
                 Varje morgon gick vi igenom status, delade upp och prioriterade
                 nya issues.
-              </p> */}
+              </p>
               <li className="font-bold">MVP!</li>
-              {/* <p>
+              <p>
                 Från första dagen bestämde vi oss för att MVP (minimum viable
                 product) var vägen att gå. Allt som inte ingick i VG-kraven
                 skulle in på en "övrigt"-lista. Det hjälpte oss att prioritera
                 vad som var viktigt och vad som var fint att ha.
-              </p> */}
+              </p>
               <li className="font-bold">Agilt arbete</li>
-              {/* <p>
+              <p>
                 Sprintar, retrospektiv och dagliga stand-ups för att hålla koll
                 på framstegen. Vi reviderade alltid dagsplan och sprintplan
                 efter behov och förmåga. Hände det något kastade vi om så det
                 passade alla.
-              </p> */}
+              </p>
             </div>
           </div>
           <div className="flex flex-col gap-8 max-w-[500px]">
@@ -165,72 +169,74 @@ const aboutUsPage = () => {
             <div className=" flex flex-col gap-2">
               <h5>Vilka lärdomar tar vi med oss?</h5>
               <li className="font-bold">Inte för många kockar!</li>
-              {/* <p>
+              <p>
                 Vi var tydliga från start att det skulle vara ordning i gruppen.
                 Alla ansvarade för att skapa och kontrollera korrekta grenar.
                 Ingen gick in i en annans kod utan att kolla med personen först.
                 Allt löstes inte samtidigt utan i prioritetsordning.
-              </p> */}
+              </p>
               <li className="font-bold">Inte jaga perfektion</li>
-              {/*  <p>
+              <p>
                 Alla har varit duktiga på att inte fastna i detaljer utan
                 fokusera på att slutföra projektet. Det var nyttigt för oss alla
                 att öva på att arbeta med begränsningar som liknar verkliga
-                projekt där tid, kvalitet och kostnad styr. Till exempel har du kanske inte
-                budget för fancy features eller tid att finjustera designen.
-              </p> */}
+                projekt där tid, kvalitet och kostnad styr. Till exempel har du
+                kanske inte budget för fancy features eller tid att finjustera
+                designen.
+              </p>
               <li className="font-bold">Grupparbete i GitHub</li>
-              {/* <p>
+              <p>
                 Superbra och effektivt att samarbeta på GitHub. Efter det här
                 projektet flyter det på bra.
-              </p> */}
+              </p>
               <li className="font-bold">Återanvända kod</li>
-              {/* <p>
+              <p>
                 Koden bör vara modulär och återanvändbar. Man tager vad man
                 haver och lånar det som fattas.
-              </p> */}
+              </p>
             </div>
             <div className=" flex flex-col gap-2">
               <h5>Vad som fungerade bra?</h5>
               <li className="font-bold">Tydlig grundstruktur</li>
-              {/* <p>
+              <p>
                 En stark grundstruktur från början gjorde arbetet smidigare. Vi
                 mappade upp hela grundstrukturen med alla komponenter och sidor
                 vi kunde komma på och döpte allt. När det väl började kodas
                 stämde namnen med det som producerades.
-              </p> */}
+              </p>
               <li className="font-bold">Bra gruppdynamik</li>
-              {/* <p>
+              <p>
                 Samarbetet i gruppen fungerade utmärkt. Tydlig och rak
                 kommunikation. Hjälpsamhet. Alla hade liknande ambitionsnivå och
                 jobbade på!
-              </p> */}
+              </p>
               <li className="font-bold">Rolig uppgift</li>
-              {/* <p>
+              <p>
                 Projektet var roligt och lärorikt. Bra med en konkret och tydlig
                 webshop.
-              </li> */}
+              </p>
             </div>
             <div className=" flex flex-col gap-2">
               <h5>Vad hade kunnat förbättras?</h5>
               <li className="font-bold">Inte mapp i en mapp!</li>
-              {/* <p>
+              <p>
                 När vi skulle städa upp kodstrukturen blev det fel i Netlify,
                 men vi kom runt det. Däremot kommer vi inte göra samma misstag i
                 framtida projekt.
-              </p> */}
+              </p>
               <li className="font-bold">Släpp inte garden!</li>
-              {/* <p>
+              <p>
                 Var noggrann genom hela projektet. Vi började slappna av lite
                 mot slutet och då uppstod några småkonflikter i koden.
-              </p> */}
+              </p>
               <li className="font-bold">Designen!</li>
-              {/* <p>
+              <p>
                 Den behövde förbättras och itereras. När man designar i Figma
                 ska man ha koden i åtanke, så att ritningen kan översättas
-                bättre till kod. Till exempel: Vikten av att designa utifrån befintlig data för att se 
-                hur långa texter etc kan vara i din design. Den hade också kunnat bli lite roligare..
-              </p> */}
+                bättre till kod. Till exempel: Vikten av att designa utifrån
+                befintlig data för att se hur långa texter etc kan vara i din
+                design. Den hade också kunnat bli lite roligare..
+              </p>
             </div>
           </div>
         </div>
